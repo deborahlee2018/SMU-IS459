6faa2fe546beb033135e6080106b555b84fc8860
Deborah Lee <deborahlee5678@gmail.com>
2021-09-12 21:08:00 +0800

    Update rdd_exercise.py
    updated to fit exercise questions

--- spark/rdd_exercise.py
+++ spark/rdd_exercise.py
@@ -13,13 +13,16 @@ score_sheet_df.show()
 score_sheet_rdd = score_sheet_df.rdd
 score_sheet_rdd.first()
 
-# Project the second column of scores with an additional 1
-score_rdd = score_sheet_rdd.map(lambda x: (x[1], 1))
+# Sort RDD in ascending order (NEW)
+score_sheet_rdd_sorted = score_sheet_rdd.sortBy(lambda x : x[1])
+
+# Remove first and last rows and Project the second column of scores with an additional 1 (EDITED)
+score_rdd = score_sheet_rdd_sorted.zipWithIndex().filter(lambda x : x[1] > 0 and x[1] < 19).map(lambda x: (x[0][1], 1))
 score_rdd.first()
 
 # Get the sum and count by reduce
-(sum, count) = score_rdd.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-print('Average Score : ' + str(sum/count))
+(total, count) = score_rdd.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+print('Average Score : ' + str(total/count))
 
 # Load Parquet file into a data frame
 posts_df = spark.read.load('/user/zzj/parquet-input/hardwarezone.parquet')
@@ -31,10 +34,15 @@ print('Jimmy has ' + str(num_post) + ' posts.')
 
 posts_rdd = posts_df.rdd
 
-# Project the author and content columns
-author_content_rdd = posts_rdd.map(lambda x: (len(x[2]), 1))
-author_content_rdd.first()
+# Project the author and content length columns
+author_content_rdd = posts_rdd.map(lambda x: (x[1], len(x[2])))
+
+# Group RDD by author
+author_content_grouped_rdd = author_content_rdd.groupByKey()
+
+# Get average post length for each author
+author_content_avg_length = author_content_grouped_rdd.map(lambda x: (x[0], sum(x[1])/len(x[1])))
 
 # Get sume and count by reduce
-(sum, count) = author_content_rdd.reduce(lambda x,y: (x[0]+y[0], x[1]+y[1]))
-print('Average post length : ' + str(sum/count))
+# (sum, count) = author_content_rdd.reduce(lambda x,y: (x[0]+y[0], x[1]+y[1]))
+# print('Average post length : ' + str(sum/count))
