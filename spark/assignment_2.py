d63c853f6fc49ef315c9cdf6177ca3856e33dada
Deborah Lee <deborahlee5678@gmail.com>
2021-09-19 13:00:25 +0800

    assignment 2

--- spark/assignment_2.py
+++ spark/assignment_2.py
@@ -2,11 +2,15 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import monotonically_increasing_id
 from graphframes import *
+from pyspark.sql.functions import desc, udf, col, lower, regexp_replace, explode, array_remove
+from pyspark.ml.feature import Tokenizer, StopWordsRemover
 
 spark = SparkSession.builder.appName('sg.edu.smu.is459.assignment2').getOrCreate()
 
 # Load data
-posts_df = spark.read.load('/user/zzj/parquet-input/hardwarezone.parquet')
+# posts_df = spark.read.load('/user/zzj/parquet-input/hardwarezone.parquet')
+posts_df = spark.read.load('/Users/Deborah/Documents/GitHub_SMU/SMU-IS459/hadoop/hardwarezone.parquet')
+
 
 # Clean the dataframe by removing rows with any null value
 posts_df = posts_df.na.drop()
@@ -62,7 +66,63 @@ graph = GraphFrame(author_id, id_to_id)
 # For complex graph queries, e.g., connected components, you need to set
 # the checkopoint directory on HDFS, so Spark can handle failures.
 # Remember to change to a valid directory in your HDFS
-spark.sparkContext.setCheckpointDir('/user/zzj/spark-checkpoint')
+spark.sparkContext.setCheckpointDir('hadoop/bin/spark-checkpoint')
 
-# The rest is your work, guys
-# ......
+# get dataframe of connected components
+sc.setCheckpointDir("/tmp/graphframes-example-connected-components")
+communities = graph.connectedComponents()
+
+# groupby and sort communities in desc order to show biggest communities
+communities_count = communities.groupBy("component").count().sort(desc("count"))
+
+# get list of top components (at least 2) in desc order 
+top_components = communities_count.rdd.filter(lambda x: x[1]>1).map(lambda x: x[0]).collect()
+
+# construct connection between author id and content posted
+posts_id_df = posts_df.join(author_id, posts_df.author == author_id.author).select(posts_df.content, author_id.id)
+
+# check for no null values
+# >>> posts_id_df.filter(posts_id_df.content.isNull()).count()
+# >>> posts_id_df.filter(posts_id_df.id.isNull()).count()
+
+# clean content
+df_clean = posts_id_df.select('id', (lower(regexp_replace('content', "[^a-zA-Z\\s]", "")).alias('content')))
+
+# tokenize content
+tokenizer = Tokenizer(inputCol='content', outputCol='words_token')
+df_words_token = tokenizer.transform(df_clean).select('id', 'words_token')
+df_cleaned_words_token = df_words_token.withColumn("cleaned_words_token", array_remove("words_token", '')).drop('words_token')
+
+# Remove stop words
+remover = StopWordsRemover(inputCol='cleaned_words_token', outputCol='words_clean')
+df_words_no_stopw = remover.transform(df_cleaned_words_token).select('id', 'words_clean')
+
+# get df with id, component, and cleaned words
+comp_df = df_words_no_stopw.join(communities, communities.id == df_words_no_stopw.id).select(df_words_no_stopw.id, df_words_no_stopw.words_clean, communities.component)
+
+# filter comp_df to show only comp 0
+comp_0_df =  comp_df.filter(comp_df.component == 0)
+
+# explode content to make a row for each word
+df_comp_0_exploded = comp_0_df.select(comp_0_df.id, explode(comp_0_df.words_clean))
+
+# get count of each unique word
+df_comp_0_word_count = df_comp_0_exploded.groupBy("col").count().sort(desc("count"))
+
+
+# filter comp_df to show only comp 0
+comp_1_df =  comp_df.filter((comp_df.component != 0) & (comp_df.component.isin(top_components)))
+
+# explode content to make a row for each word
+df_comp_1_exploded = comp_1_df.select(comp_1_df.id, explode(comp_1_df.words_clean))
+
+# get count of each unique word
+df_comp_1_word_count = df_comp_1_exploded.groupBy("col").count().sort(desc("count"))
+
+
+# create graph of comp 0
+author_id_comp_0 = communities.filter(communities.component == 0)
+graph_comp_0 = GraphFrame(author_id_comp_0, id_to_id)
+
+# get triangle count
+triangle_count = graph_comp_0.triangleCount()
